76fce965050788fedfed143f8ced56483c533ae3
theChosenDevop <tobbey.adesanya@gmail.com>
2023-10-04 11:34:48 -0700

    update

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -1,9 +1,3 @@
 #!/usr/bin/python3
 def pow(a, b):
-    if b < 0:
-        a = 1 / a
-        b = -b
-    result = 1
-    for _ in range(b):
-        result *= a
-    return round(result)
+    return a ** b
