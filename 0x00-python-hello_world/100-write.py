49c0503c2dac5b9e444ee7405d2b98f57501433a
theChosenDevop <tobbey.adesanya@gmail.com>
2023-10-04 16:33:41 -0700

    update write

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 import sys
-nWrite = sys.stdout.write("and that piece of art is useful - Dora korpar, 2015-10-19\n")
-if (nWrite == -1):
-    sys.stderr.write("Error\n")
-else:
-    sys.exit(1)
+sys.stderr.write("and that piece of art is useful - Dora korpar, 2015-10-19\n")
+sys.exit(1)
