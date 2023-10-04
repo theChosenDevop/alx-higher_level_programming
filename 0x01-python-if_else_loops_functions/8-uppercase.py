70d1fe4f23d57299d2b6c0bedf7adab9e7e09664
theChosenDevop <tobbey.adesanya@gmail.com>
2023-10-04 09:19:19 -0700

    def_isuppercase

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def uppercase(str):
+    result = ''
+    for char in str:
+        if 'a' <= char <= 'z':
+            upper_case = chr(ord(char) - 32)
+            result += upper_case
+        else:
+            result += char
+    print(result)
