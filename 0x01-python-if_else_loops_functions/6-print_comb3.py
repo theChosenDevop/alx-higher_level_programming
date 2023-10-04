b87b8d14ca3ec6279f964c75f25b2d7251027b83
theChosenDevop <tobbey.adesanya@gmail.com>
2023-10-04 07:20:33 -0700

    update print_integer_combinations

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,7 +2,6 @@
 for i in range(0, 10):
     for j in range(i + 1, 10):
         if i == 8 and j == 9:
-            print(89)
-            continue
-        print(i, end='')
-        print(j, end=', ')
+            print("{}{}".format(i, j))
+        else:
+            print("{0}{1}, ".format(i, j), end='')
