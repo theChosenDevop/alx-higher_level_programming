cbfc153cdc398271a63cf8dc22304dd622bc484b
theChosenDevop <tobbey.adesanya@gmail.com>
2023-10-05 17:09:51 -0700

    calculation function

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    """
+        calculate the sum, multiplication,
+            subtraction and division of a and b
+    """
+    from calculator_1 import add, sub, mul, div
+
+    a = 12
+    b = 5
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} - {} = {}".format(a, b, sub(a, b)))
+    print("{} * {} = {}".format(a, b, mul(a, b)))
+    print("{} / {} = {}".format(a, b, div(a, b)))
