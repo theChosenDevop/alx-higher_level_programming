ad9ab6b20f153fedfd0e9e6a2b647a6a7ce7d972
theChosenDevop <tobbey.adesanya@gmail.com>
2023-10-05 19:31:23 -0700

    print module variable

---
+++ 0x02-python-import_modules/5-variable_load.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    """
+        program imports the variable a from variable_load_5 module
+        prints its value
+    """
+    from variable_load_5 import a
+
+    print(a)
