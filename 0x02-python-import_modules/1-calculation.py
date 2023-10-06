bbd38d597464cfab7bb48c8c9157761adbf0840b
theChosenDevop <tobbey.adesanya@gmail.com>
2023-10-05 17:15:36 -0700

    calculation function

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+
 if __name__ == "__main__":
     """
         calculate the sum, multiplication,
@@ -6,8 +7,9 @@ if __name__ == "__main__":
     """
     from calculator_1 import add, sub, mul, div
 
-    a = 12
+    a = 10
     b = 5
+
     print("{} + {} = {}".format(a, b, add(a, b)))
     print("{} - {} = {}".format(a, b, sub(a, b)))
     print("{} * {} = {}".format(a, b, mul(a, b)))
