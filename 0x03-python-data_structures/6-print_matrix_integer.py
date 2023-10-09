5aae85a340d46a2cc75c4f15b479f4aa20666fea
theChosenDevop <tobbey.adesanya@gmail.com>
2023-10-09 02:27:36 -0700

    defined print_matrix_intger function

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# 6-print_matrix_integer.py
+
+def print_matrix_integer(matrix=[[]]):
+    """print matrix function
+        Args:
+            matrix: list of 3 x 3 integers
+
+        Return: rows
+    """
+    for row in matrix:
+        for col in row:
+            print("{:d}".format(col), end=" ")
+        print()
