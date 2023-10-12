e9c19acc58e3b106e55b1e1c24ec586188112a1c
theChosenDevop <tobbey.adesanya@gmail.com>
2023-10-11 17:19:40 -0700

    set intersect function

---
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# 3-common_elements.py
+
+def common_elements(set_1, set_2):
+    """
+        Args:
+            set_1: first set
+            set_2: second set
+
+        Returns:
+            Intersect of two sets
+    """
+    return set_1 & set_2
