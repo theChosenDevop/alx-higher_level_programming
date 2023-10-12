29dab32046a9587fbf97a9318103c9b1a10224a3
theChosenDevop <tobbey.adesanya@gmail.com>
2023-10-11 18:55:27 -0700

    update dictionary function

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,15 @@
+#!/usr/bin/pythpn3
+# 7-update_dictionary.py
+
+def update_dictionary(a_dictionary, key, value):
+    """upadate dictionary function
+
+    Args:
+        a_dictionary: dictionary
+        key: dictionary key
+        value : dicticionary value
+    Returns:
+        updated dictionary
+    """
+    a_dictionary[key] = value
+    return a_dictionary
