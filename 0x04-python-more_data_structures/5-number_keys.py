c31d7743677e51b9acaf89d88a21e21662575689
theChosenDevop <tobbey.adesanya@gmail.com>
2023-10-11 17:42:39 -0700

    number of keys function

---
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# 5-number_keys.py
+
+def number_keys(a_dictionary):
+    """
+        Args:
+            a_dictionary: dictionary argument
+
+        Returns:
+            number of keys
+    """
+    return len(a_dictionary.keys())
