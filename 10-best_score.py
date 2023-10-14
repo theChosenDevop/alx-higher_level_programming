650a0582f5348aedbbba38249cb2bbbd4364d6a9
theChosenDevop <tobbey.adesanya@gmail.com>
2023-10-13 19:27:32 -0700

    multiply function

--- 10-best_score.py
+++ 10-best_score.py
@@ -11,7 +11,7 @@ def best_score(a_dictionary):
     """
     max_val = 0
     best_key = None
-    if  not a_dictionary:
+    if not a_dictionary:
         return None
     for key, value in a_dictionary.items():
         if value > max_val:
