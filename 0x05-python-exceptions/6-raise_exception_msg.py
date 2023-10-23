40ae3e9df7c6cb42daa346ce4f74657ce83fe815
theChosenDevop <tobbey.adesanya@gmail.com>
2023-10-23 11:46:23 -0700

    raise NameError

---
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def raise_exception_msg(message=""):
+    """
+        raise_exception_msg
+            function that raises a name exception with a message.
+
+        Args:
+            message: input
+
+        Return: message
+    """
+    raise NameError(message)
