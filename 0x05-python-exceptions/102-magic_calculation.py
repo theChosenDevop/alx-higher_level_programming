13d58cea796dcb5e98dbbb6c19316ccc0de134f0
theChosenDevop <tobbey.adesanya@gmail.com>
2023-10-23 14:56:11 -0700

    magic_calculation function

---
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+def magic_calculation(a, b):
+    """
+        magic_calculation
+            calculates a and b
+    """
+    result = 0
+    for i in range(1, 3):
+        try:
+            if i > a:
+                raise Exception("Too far")
+            else:
+                result += a **b / i
+        except Exception:
+            result = b + a
+            break 
+    return result
+
