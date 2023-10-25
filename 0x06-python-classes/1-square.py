deef8d37305d70a2274ec35fd7ea4212888cf26a
theChosenDevop <tobbey.adesanya@gmail.com>
2023-10-25 13:54:56 -0700

    initialized attribute

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" definition of a class Square """
+
+
+class Square:
+    """
+        representation of square
+    """
+    def __init__(self, size):
+        """" initialize attributes """
+        self._size = size
