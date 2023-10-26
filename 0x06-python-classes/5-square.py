a36e41f37434f32bd05b8754be253bf5c1431f2a
theChosenDevop <tobbey.adesanya@gmail.com>
2023-10-26 13:54:13 -0700

    defined public instance method my_print

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+""" definition of a class Square """
+
+
+class Square:
+    """
+        representation of square
+    """
+    def __init__(self, size=0):
+        """" initialize attributes
+
+            Args:
+                size: an integer of square
+        """
+        self.size = size
+
+    def area(self):
+        """ Returns: the current square area """
+        return self.__size * self.__size
+
+    @property
+    def size(self):
+        """ Returns the size of square class """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def my_print(self):
+        """ prints in stdout the square """
+        for element in range(self.__size):
+            [print("#", end="") for i in range(0, self.__size)]
+            print()
+        else:
+            print()
