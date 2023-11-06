7f7a91f0a70280c7035510386313291d5ddb1197
theChosenDevop <tobbey.adesanya@gmail.com>
2023-11-06 14:59:50 -0800

    defined class checker function

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" Defines function to check if class is instance of type class """
+
+
+def is_same_class(obj, a_class):
+    """"
+        obj: object to check class
+        a_class: class type
+    """
+    return isinstance(obj, a_class)
