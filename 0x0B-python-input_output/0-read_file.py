9962424a0ac8fb18711e235893e0bfc435f17a9a
theChosenDevop <tobbey.adesanya@gmail.com>
2023-11-07 09:07:16 -0800

    defined read_file function

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """ Defines read_file function """
 
+
 def read_file(filename=""):
     """ reads a text file(UTF8) and prints it to stdout """
     with open(filename, encoding="utf-8") as f:
-        f.read()
+        print(f.read(), end="")
