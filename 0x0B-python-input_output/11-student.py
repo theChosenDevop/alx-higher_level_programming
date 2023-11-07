1bd7f364d27c318250cfcb516bbd98e1f99b3fd1
theChosenDevop <tobbey.adesanya@gmail.com>
2023-11-07 13:37:29 -0800

    reload_from_json function

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+""" defines a class Student """
+
+
+class Student:
+    """"A class student"""
+
+    def __init__(self, first_name, last_name, age):
+        """
+            first_name: user first name
+            last_name: user last name
+            age: user age
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """ returns a dictionary representation of object """
+        if attrs is None:
+            return self.__dict__
+
+        json_data = {}
+        for attr in attrs:
+            if hasattr(self, attr):
+                json_data[attr] = getattr(self, attr)
+        return json_data
+
+    def reload_from_json(self, json):
+        """ replaces all attributes of the Student instance """
+        for key, value in json.items():
+            setattr(self, key, value)
