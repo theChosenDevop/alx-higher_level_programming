46017aac431f529410707fefa78a662326baa2ef
theChosenDevop <tobbey.adesanya@gmail.com>
2023-11-07 19:00:35 -0800

    updated function

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -12,4 +12,4 @@ def append_after(filename="", search_string="", new_string=""):
         for line in lines:
             f.write(line)
             if search_string in line:
-                f.write(new_string + '\n')
+                f.write(new_string)
