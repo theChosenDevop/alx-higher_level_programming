501954ff68863cf4a703f91699cb9768a0a89e09
theChosenDevop <tobbey.adesanya@gmail.com>
2023-11-14 04:54:38 -0800

    updated attributes height and width exception

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -40,7 +40,7 @@ class Rectangle(Base):
         """ Set the width attribute of the object"""
         if type(value) is not int:
             raise TypeError("width must be an integer")
-        if value < 0:
+        if value <= 0:
             raise ValueError("width must be > 0")
         self.__width = value
 
@@ -54,7 +54,7 @@ class Rectangle(Base):
         """ Set the height of the object """
         if type(value) is not int:
             raise TypeError("height must be an integer")
-        if value < 0:
+        if value <= 0:
             raise ValueError("height must be > 0")
         self.__height = value
 
