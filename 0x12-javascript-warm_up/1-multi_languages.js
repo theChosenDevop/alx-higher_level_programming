707a44152fa9e380bb55b9bea8e40a6e3f449dbd
theChosenDevop <tobbey.adesanya@gmail.com>
2024-01-08 12:04:26 -0800

    javascript

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+let first_var = 'C is fun';
+let sec_var = 'Python is cool';
+let third_var = 'Javascript is amazing';
+
+console.log(first_var);
+console.log(sec_var);
+console.log(third_var);
