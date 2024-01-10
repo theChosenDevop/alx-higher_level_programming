b549e08e73ae52f602ac4b1737c3648f776aa471
theChosenDevop <tobbey.adesanya@gmail.com>
2024-01-10 07:08:28 -0800

    concat files in JS

--- 0x13-javascript_objects_scopes_closures/7-main.js
+++ 0x13-javascript_objects_scopes_closures/7-main.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
-const nb0ccurences = require('./7-occurrences').nb0ccurences;
+const nbOccurences = require('./7-occurrences').nbOccurences;
 
-console.log(nb0ccurences([1, 2, 3, 4, 5, 6], 3));
-console.log(nb0ccurences([3, 2, 3, 4, 5, 3, 3], 3));
-console.log(nb0ccurences(["S", 12, "c", "S", "School", 8], "S"));
+console.log(nbOccurences([1, 2, 3, 4, 5, 6], 3));
+console.log(nbOccurences([3, 2, 3, 4, 5, 3, 3], 3));
+console.log(nbOccurences(["S", 12, "c", "S", "School", 8], "S"));
--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 /* A function that returns the number of occurrences in a list */
 
-exports.nb0ccurences = function nb0ccurences (list, searchElement) {
+exports.nbOccurences = function (list, searchElement) {
   let noccurence = 0;
 
   for (let i = 0; i < list.length; i++) {
