4912358c8c7ac47e8518e9130fd8d883ef2b5823
theChosenDevop <tobbey.adesanya@gmail.com>
2024-01-14 20:26:05 -0800

    update

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-""" A script that prints the first State object
-    from the database hbtn_0e_6_usa
+"""
+    A script that lists all State objects that contain
+    the letter a from the database
 """
 import sys
 from sqlalchemy.orm import sessionmaker
@@ -11,20 +12,10 @@ from model_state import Base, State
 if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]))
-
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    try:
-        oneState = session.query(State).first()
-
-        if oneState is None:
-            print('Nothing')
-        else:
-            print(oneState.id, oneState.name, sep=": ")
-    except Exception as e:
-        print("Error:", e)
-
-    finally:
-        session.close()
+    """ Query State objects where name starts with A """
+    state = session.query(State).filter(State.name.like('B%')).all()
+    print(state.id, state.name, sep=": ")
