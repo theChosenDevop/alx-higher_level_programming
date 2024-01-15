c1c49295253ff7392f8a8cefc045a6381720fb3b
theChosenDevop <tobbey.adesanya@gmail.com>
2024-01-15 03:26:05 -0800

    Get a state

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
-    A script that lists all State objects that contain
-    the letter a from the database
+    A script that prints the State object with the name
+    passed as argument from the database hbtn_0e_6_usa
 """
 import sys
 from sqlalchemy.orm import sessionmaker
@@ -12,16 +12,14 @@ from model_state import Base, State
 if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]))
-
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    """ Query State objects where name starts with A """
-    states = (
-       session.query(State)
-       .filter(State.name.like('%a%'))
-       .order_by(State.id).all()
-       )
-    for state in states:
-        print(state.id, state.name, sep=": ")
+    state = session.query(State).filter(State.name == (sys.argv[4],))
+
+    try:
+        print(state[0].id)
+    except IndexError:
+        print("Not found")
+    session.close()
--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -3,6 +3,7 @@
 import sys
 import MySQLdb
 
+
 if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3], port=3306)
