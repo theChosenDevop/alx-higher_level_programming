e0c99aac188a6947bf0be8fd4c9f9aa3362c3066
theChosenDevop <tobbey.adesanya@gmail.com>
2024-01-14 18:12:08 -0800

    sqlalchemy database

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" A script that takes in the name of a state
+    as an argument and lists all cities of that state,
+    using the database hbtn_0e_4_usa
+"""
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    state = sys.argv[4]
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1], passwd=sys.argv[2],
+                         db=sys.argv[3], port=3306)
+    cursor = db.cursor()
+    cursor.execute("""SELECT cities.name FROM cities INNER JOIN states
+                   ON states.id = cities.state_id WHERE states.name = %s""", (state, ))
+    rows = cursor.fetchall()
+    result = ', '.join(row[0] for row in rows)
+    print(result)
+    cursor.close()
+    db.close()
