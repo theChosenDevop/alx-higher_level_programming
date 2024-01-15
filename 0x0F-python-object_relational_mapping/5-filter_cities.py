2aeb73be126443feb5b5ff8ab073280655fce3b5
theChosenDevop <tobbey.adesanya@gmail.com>
2024-01-14 19:01:20 -0800

    All states via SQLAlchemy

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -8,11 +8,12 @@ import MySQLdb
 
 if __name__ == "__main__":
     state = sys.argv[4]
-    db = MySQLdb.connect(host="localhost", user=sys.argv[1], passwd=sys.argv[2],
-                         db=sys.argv[3], port=3306)
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2],db=sys.argv[3], port=3306)
     cursor = db.cursor()
-    cursor.execute("""SELECT cities.name FROM cities INNER JOIN states
-                   ON states.id = cities.state_id WHERE states.name = %s""", (state, ))
+    cursor.execute("""SELECT cities.name FROM cities
+                   INNER JOIN states ON states.id = cities.state_id
+                   WHERE states.name = %s""", (state, ))
     rows = cursor.fetchall()
     result = ', '.join(row[0] for row in rows)
     print(result)
---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" A script that lists all State objects from the database hbtn_0e_6_usa """
+import sys
+from model_state import State, Base
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(sys.argv[1], sys.argv[2],
+                           sys.argv[3]))
+    Base.metadata.create_all(engine)
+    Session = sessionmaker()
+    Session.configure(bind=engine)
+    session = Session()
+    states = session.query(State).order_by(State.id).all()
+
+    for state in states:
+        print('{}: {}'.format(state.id, state.name))
+
+    session.close()
