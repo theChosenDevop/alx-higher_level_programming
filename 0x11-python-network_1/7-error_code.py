d386bb9fec179e703ea44a35d3e78cc6fd406e5a
theChosenDevop <tobbey.adesanya@gmail.com>
2024-01-26 19:37:26 -0800

    print requests module error

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Script takes in a URL, sends a request to the URL
+    and displays the body of the response.
+"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+
+    reqBody = requests.get(url).text
+    if reqBody.status_code >= 400:
+        print("Error code: {}".format(reqBody.status_code))
+    else:
+        print(reqBody)
