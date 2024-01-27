a7d68aa275085e0e60d8a88e30fe1bf2bf04a91b
theChosenDevop <tobbey.adesanya@gmail.com>
2024-01-26 17:57:33 -0800

    get response_header_id using urllib

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -5,8 +5,9 @@ import urllib.request
 """This module is used to work with urls for html requests & responses"""
 
 
+url = "https://alx-intranet.hbtn.io/status"
 if __name__ == "__main__":
-    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
+    with urllib.request.urlopen(url) as response:
         html = response.read()
         print("Body response:")
         print("\t- type: {}".format(type(html)))
---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" Script takes in a URL,sends a request to the URL and
+    displays the value of the X-Request-Id variable found in
+    the header of the response
+"""
+import urllib.request
+import sys
+
+
+url = sys.argv[1]
+if __name__ == "__main__":
+    with urllib.request.urlopen(url) as response:
+        print(response.headers.get('X-Request-Id'))
