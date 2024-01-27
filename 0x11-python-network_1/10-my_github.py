b30f0ebef3db6f8c04f91b2a5d93273b52da0081
theChosenDevop <tobbey.adesanya@gmail.com>
2024-01-26 20:01:53 -0800

    search API

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+""" Python script that takes your GitHub credentials (username and
+    password) and uses the GitHub API to display your id
+"""
+
--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -2,13 +2,14 @@
 """ Python script that takes in a letter and sends a POST request to
     http://0.0.0.0:5000/search_user with the letter as a parameter.
 """
+
 import sys
 import requests
 
 
 if __name__ == "__main__":
     url = "http://0.0.0.0:5000/search_user"
-    letter = sys.argv[1] if  len(sys.argv) > 1 else ""
+    letter = sys.argv[1] if len(sys.argv) > 1 else ""
     data = {'q': letter}
 
     response = requests.post(url, data=data)
