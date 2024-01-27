b2c3a64b750739969ee9a19ffa20c0ab9b54821c
theChosenDevop <tobbey.adesanya@gmail.com>
2024-01-26 20:12:02 -0800

    fetch data with auth

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -3,3 +3,14 @@
     password) and uses the GitHub API to display your id
 """
 
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    passwd = sys.argv[2]
+    url = 'https://api.github.com/user'
+    response = requests.get(url, auth=(username, passwd))
+    json_data = response.json()
+    print(json_data.get('id'))
