a9846d4b500ee95ffa9470ae4053928913b30536
theChosenDevop <tobbey.adesanya@gmail.com>
2024-02-20 07:38:22 -0800

    update

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,16 +1,11 @@
 #!/usr/bin/node
-/*Write to file from commandline*/
 
 const fs = require('fs');
-
-if (process.argv.length !== 4) {
-  console.error("Usage: ./writeme <filename> <data>");
-}
 
 fs.writeFile(process.argv[2], process.argv[3], 'utf-8', (err, data) => {
   if (err) {
     console.error(err);
   } else {
-    console.log("Data has been written to file successfully");
+    console.log('Data has been written to file successfully');
   }
 });
