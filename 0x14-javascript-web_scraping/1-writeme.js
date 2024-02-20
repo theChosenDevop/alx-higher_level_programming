68897817ac8750cd931e7bd4dd80fbe18d006d6e
theChosenDevop <tobbey.adesanya@gmail.com>
2024-02-20 07:29:49 -0800

    write to file

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+/*1-writeme module*/
+
+const fs = require('fs');
+
+if (process.argv.length !== 4) {
+  console.error("Usage: ./writeme <filename> <data>");
+}
+
+fs.writeFile(process.argv[2], process.argv[3], (err, data) => {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log("Data has been written to file successfully");
+  }
+});
